681a6168b0aad4fa1387a5151e45edf3fd828ebb
VyshnaviKR <vvyshnavi776@gmail.com>
2026-07-16 11:01:58 +0530

    Second commit

--- TNSIF_training/src/org/tnsif/acce/c2t/javabasics/VariableDemo.java
+++ TNSIF_training/src/org/tnsif/acce/c2t/javabasics/VariableDemo.java
@@ -1,26 +1,35 @@
 package org.tnsif.acce.c2t.javabasics;
 
-public class VariableDemo {
-	int price=10;//instance var
-	void print()//method
-	{
-		String msg ="hello";//local var
-		System.out.println(msg);
+
+
+	public class VariableDemo {
+		int price=10;//instance var
+		void print()//method
+		{
+			String msg ="hello";//local var
+			System.out.println(msg);
+			
+		}
+		static String message="hello students";
 		
-	}
-	static String message="hello students";
+
+		public static void main(String[] args) {
+			VariableDemo obj = new VariableDemo();
+			System.out.println(obj.price);
+			obj.print();
+			System.out.println(message);
+			
+			
+			
+			// TODO Auto-generated method stub
+
+		}
+
 	
 
-	public static void main(String[] args) {
-		VariableDemo obj = new VariableDemo();
-		System.out.println(obj.price);
-		obj.print();
-		System.out.println(message);
-		
-		
-		
+
 		// TODO Auto-generated method stub
 
 	}
 
-}
+

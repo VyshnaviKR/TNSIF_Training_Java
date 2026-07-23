9ce0f821505546bfcd2295459837127124de6209
VyshnaviKR <vvyshnavi776@gmail.com>
2026-07-23 15:03:42 +0530

    First commit

--- TNSIF_training/src/org/tnsif/acc/c2tc/oops/FoodItem.java
+++ TNSIF_training/src/org/tnsif/acc/c2tc/oops/FoodItem.java
@@ -1,36 +1,29 @@
 package org.tnsif.acc.c2tc.oops;
 
 public class FoodItem {
-	String name;
-	double price;
-	String category;
-	
-	//method
-	void display() {
-		System.out.println("food name : " +name );
-		System.out.println("price : " +price );
-		System.out.println("category : " +category );
-		
-	}
+    String name;
+    double price;
+    String category;
 
-	public static void main(String[] args) {
-		FoodItem fooditem=new FoodItem();
-		fooditem.display();
-		fooditem.name="Pizza";
-		fooditem.price=299.99;
-		fooditem.category="fast food";
-		 
-		
-		FoodItem fooditem2=new FoodItem();
-		fooditem2.display();
-		fooditem2.name="Sushi;
-		fooditem2.price=599.99;
-		fooditem2.category="Japan Cusine";
-		
-		
-		
-		// TODO Auto-generated method stub
+    void display() {
+        System.out.println("Food Name : " + name);
+        System.out.println("Price : " + price);
+        System.out.println("Category : " + category);
+        System.out.println();
+    }
 
-	}
+    public static void main(String[] args) {
 
+        FoodItem fooditem1 = new FoodItem();
+        fooditem1.name = "Pizza";
+        fooditem1.price = 299.99;
+        fooditem1.category = "Fast Food";
+        fooditem1.display();
+
+        FoodItem fooditem2 = new FoodItem();
+        fooditem2.name = "Sushi";
+        fooditem2.price = 599.99;
+        fooditem2.category = "Japanese Cuisine";
+        fooditem2.display();
+    }
 }
